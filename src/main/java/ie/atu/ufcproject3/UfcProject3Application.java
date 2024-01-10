2e24490f2f2c25c5674d8a6398b907bae396987b
denis <g00403126@atu.ie>
2024-01-10 20:50:58 +0000

    ..

---
+++ src/main/java/ie/atu/ufcproject3/UfcProject3Application.java
@@ -0,0 +1,13 @@
+package ie.atu.ufcproject3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfcProject3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UfcProject3Application.class, args);
+    }
+
+}
